1cd544f243d96b17ab04a687207e388507298352
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2023-10-06 20:22:06 -0300

    bgls

--- exs/5ªL - funções de lista/ex1.py
+++ exs/5ªL - funções de lista/ex1.py
@@ -1,6 +1,11 @@
-from functools import reduce
+# from functools import reduce
+
+# def soma_nat(n):
+#     return reduce(lambda x, y: x + y, range(1, n + 1))
+
+# print(soma_nat(6))
 
 def soma_nat(n):
-    return reduce(lambda x, y: x + y, range(1, n + 1))
-
-print(soma_nat(5))
+    return sum(range(1, n+1))
+    
+print(soma_nat(6))
