ef9e40292bfedb4c348e92b123c82737ba4e3882
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2023-10-06 20:06:30 -0300

    bgls

--- listas tuplas dicionários conjuntos/3-map exemplo.py
+++ listas tuplas dicionários conjuntos/3-map exemplo.py
@@ -1,3 +1,6 @@
+from functools import reduce
+from operator import add
+
 # map(função, iterável)
 # map(oq fazer, alguma lista ou string)
 
@@ -11,3 +14,20 @@ print(lista)
 
 valores = [-1, -34, 45, 345, 45, 2, -6]
 print(list(filter(lambda x: x > 0, valores)))
+
+def add():
+  pass
+
+def pares(x):
+  return x % 2 == 0
+
+A = ['1', '2', '3', '4']
+B = list(map(int, A))
+print(A)
+print(B)
+C = list(filter(pares, B))
+C = list(filter(lambda x: x % 2 == 0, B))
+print(C)
+
+soma = reduce(add, B)
+print('Soma: ', soma)
