6fb92dfe46f8e50e65626228449a6d22a1f851bc
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2023-10-06 20:43:53 -0300

    bgls

--- exs/5ªL - funções de lista/ex2.py
+++ exs/5ªL - funções de lista/ex2.py
@@ -1,7 +1,12 @@
-def quadrado(x):
-    return x ** 2
+# def quadrado(x):
+#     return x ** 2
+
+# def quadrados(n):
+#     return list(map(quadrado, range(1, n + 1)))
+
+# print(quadrados(20))
 
 def quadrados(n):
-    return list(map(quadrado, range(1, n + 1)))
+    return list(map(lambda x: x ** 2, range(1, n + 1)))
 
 print(quadrados(20))
--- exs/5ªL - funções de lista/ex3.py
+++ exs/5ªL - funções de lista/ex3.py
@@ -1,6 +1,6 @@
 w = [3, 2, 5, 6, 2, 3, 5]
 
 def indices_pares(w):
-    return list(map(lambda i: w[i], range(0, len(w), 2)))
+    return list(map(lambda i: w[i], range(1, len(w), 2)))
 
 print(indices_pares(w))
