c419b0f7dc8ffb8008c1acef5a267b77de48ddd7
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2023-10-27 20:24:46 -0300

    10/27

--- exs/6ªL - dicionários/ex2.py
+++ exs/6ªL - dicionários/ex2.py
@@ -1,24 +1,20 @@
 from time import sleep
 
-def en_to_br():
-  palavra = input(f'''\n{"="*60}\n
-Inglês para Português
-
-Palavra -> ''')
-  if palavra in tradEN:
-    print(f'{palavra} = {tradEN.get(palavra)}')
-    sleep(1)
-  else:
-    print(f'Palavra {palavra} não encontrada.')
-    sleep(1)
-
-def br_to_en():
-  palavra = input(f'''\n{"="*60}\n
+def traduzir(lang):
+  
+  if str(lang) == 'tradBR':
+    palavra = input(f'''\n{"="*60}\n
 Português para inglês
 
 Palavra -> ''')
-  if palavra in tradBR:
-    print(f'{palavra} = {tradBR.get(palavra)}')
+  else:
+    palavra = input(f'''\n{"="*60}\n
+Inglês para Português
+
+Palavra -> ''')
+    
+  if palavra in lang:
+    print(f'{palavra} = {lang.get(palavra)}')
     sleep(1)
   else:
     print(f'Palavra {palavra} não encontrada.')
@@ -32,66 +28,10 @@ def quit():
 
 if __name__ == '__main__':
   while True:
-    tradBR = {
-    'arroz': 'rice',
-    'feijão': 'bean',
-    'alho': 'garlic',
-    'macarrão': 'pasta',
-    'carne': 'meat',
-    'peixe': 'fish',
-    'frango': 'chicken',
-    'batata': 'potato',
-    'cenoura': 'carrot',
-    'tomate': 'tomato',
-    'abacaxi': 'pineapple',
-    'banana': 'banana',
-    'morango': 'strawberry',
-    'uva': 'grape',
-    'melancia': 'watermelon',
-    'pão': 'bread',
-    'queijo': 'cheese',
-    'ovos': 'eggs',
-    'leite': 'milk',
-    'manteiga': 'butter',
-    'açúcar': 'sugar',
-    'sal': 'salt',
-    'pimenta': 'pepper',
-    'cebola': 'onion',
-    'salsicha': 'sausage',
-    'azeite': 'olive oil',
-    'vinagre': 'vinegar',
-    'mostarda': 'mustard'
-    }
+    tradBR = { 'arroz': 'rice', 'feijão': 'bean', 'alho': 'garlic', 'macarrão': 'pasta', 'carne': 'meat', 'peixe': 'fish', 'frango': 'chicken', 'batata': 'potato', 'cenoura': 'carrot', 'tomate': 'tomato', 'abacaxi': 'pineapple', 'banana': 'banana', 'morango': 'strawberry', 'uva': 'grape', 'melancia': 'watermelon', 'pão': 'bread', 'queijo': 'cheese', 'ovos': 'eggs', 'leite': 'milk', 'manteiga': 'butter', 'açúcar': 'sugar', 'sal': 'salt', 'pimenta': 'pepper', 'cebola': 'onion', 'salsicha': 'sausage', 'azeite': 'olive oil', 'vinagre': 'vinegar', 'mostarda': 'mustard'
+    } 
 
-    tradEN = {
-    'rice': 'arroz',
-    'bean': 'feijão',
-    'garlic': 'alho',
-    'pasta': 'macarrão',
-    'meat': 'carne',
-    'fish': 'peixe',
-    'chicken': 'frango',
-    'potato': 'batata',
-    'carrot': 'cenoura',
-    'tomato': 'tomate',
-    'pineapple': 'abacaxi',
-    'banana': 'banana',
-    'strawberry': 'morango',
-    'grape': 'uva',
-    'watermelon': 'melancia',
-    'bread': 'pão',
-    'cheese': 'queijo',
-    'eggs': 'ovos',
-    'milk': 'leite',
-    'butter': 'manteiga',
-    'sugar': 'açúcar',
-    'salt': 'sal',
-    'pepper': 'pimenta',
-    'onion': 'cebola',
-    'sausage': 'salsicha',
-    'olive oil': 'azeite',
-    'vinegar': 'vinagre',
-    'mustard': 'mostarda'
+    tradEN = { 'rice': 'arroz', 'bean': 'feijão', 'garlic': 'alho', 'pasta': 'macarrão', 'meat': 'carne', 'fish': 'peixe', 'chicken': 'frango', 'potato': 'batata', 'carrot': 'cenoura', 'tomato': 'tomate', 'pineapple': 'abacaxi', 'banana': 'banana', 'strawberry': 'morango', 'grape': 'uva', 'watermelon': 'melancia', 'bread': 'pão', 'cheese': 'queijo', 'eggs': 'ovos', 'milk': 'leite', 'butter': 'manteiga', 'sugar': 'açúcar', 'salt': 'sal', 'pepper': 'pimenta', 'onion': 'cebola', 'sausage': 'salsicha', 'olive oil': 'azeite', 'vinegar': 'vinagre', 'mustard': 'mostarda'
     }
 
     op = input(f'''\n{"="*60}\n\nEscolha uma opção:
@@ -100,5 +40,5 @@ if __name__ == '__main__':
 3. Sair
 Opção -> ''')
     
-    br_to_en() if op == '1' else en_to_br() if op == '2' else quit() if op =='3' else None
+    traduzir(tradBR) if op == '1' else traduzir(tradEN) if op == '2' else quit() if op =='3' else None
   
